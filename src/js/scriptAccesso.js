6f6ca1cea8f755ba2c7e4847ca64a46d32d8522c
Nikzeal <nicolo.lacara@aldini.istruzioneer.it>
2023-04-10 00:43:09 +0200

    sistemata pagina di accesso

--- src/js/scriptAccesso.js
+++ src/js/scriptAccesso.js
@@ -3,42 +3,36 @@ document.addEventListener('DOMContentLoaded', init() );
 function init() {
 
 	// Riferimenti agli elementi HTML
-	const form = document.getElementById('form_accesso');
+	const form = document.getElementById("form_accesso");
 	const usernameInput = document.getElementById('username');
 	const passwordInput = document.getElementById('password');
 	const errorLabel = document.getElementById('msg_errore');
 
 	// Aggiunta di un ascoltatore per l'invio del form
-	form.addEventListener('submit', accedi);
-	
-	
-	function accedi()  {
-	
-		// Verifica dei dati inseriti
-		/*if (usernameInput.value.trim() === '') {
-			showError('Inserisci lo username');
-			return;
-		}else*/ if(usernameInput.value.trim().includes(' ')){
-			showError("L'username non puo' contenere spazi");
-			return;	
-			//Altri controlli USERNAME
-		}
-		/*if (passwordInput.value.trim() === '') {
-			showError('Inserisci la password');
-			return;
-			//Altri controlli PASSWORD
-		}*/
-	
-		// Invio del form se i dati sono validi
-		else{
+	form.addEventListener("submit", controlli_input.bind(null, form, usernameInput, passwordInput, errorLabel) );
 
-			form.submit();
-		}
-	}
-
-	// Funzione per visualizzare un messaggio di errore
-	function showError(message) {
-		errorLabel.innerHTML = message;
-	}
 
 }
+
+
+function controlli_input(form, usernameInput, passwordInput, errorLabel)  {
+	
+		// Verifica campi non vuoti (non necessario)
+		if (usernameInput.value.trim() === '' || passwordInput.value.trim() === '') {
+			//showError('I campi sono obbligatori'); 
+			errorLabel.innerHTML = 'I campi sono obbligatori';
+			return;
+		}
+		//verifica spazi nell'username
+		else if(usernameInput.value.trim().includes(' ')){
+			//showError("L'username non puo' contenere spazi");
+			errorLabel.innerHTML = "L'username non puo' contenere spazi";
+			return;	
+		}
+		
+}
+
+/* Funzione per visualizzare un messaggio di errore. Non trovo necessario scrivere una funzione per stampare un messaggio
+	function showError(message) {
+		errorLabel.innerHTML = message;
+	}*/
