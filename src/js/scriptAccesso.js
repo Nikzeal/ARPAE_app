6bff2844f281cf1ef8b3644bd86b23917b4eba24
Nikzeal <nicolo.lacara@aldini.istruzioneer.it>
2023-04-10 00:22:16 +0200

    piccola modifica

--- src/js/scriptAccesso.js
+++ src/js/scriptAccesso.js
@@ -1,40 +1,44 @@
-document.addEventListener('DOMContentLoaded', attivaEventi );
+document.addEventListener('DOMContentLoaded', init() );
 
-function attivaEventi() {
+function init() {
 
 	// Riferimenti agli elementi HTML
-	const form = document.querySelector('form');
-	const usernameInput = document.querySelector('input[name="username"]');
-	const passwordInput = document.querySelector('input[name="password"]');
+	const form = document.getElementById('form_accesso');
+	const usernameInput = document.getElementById('username');
+	const passwordInput = document.getElementById('password');
 	const errorLabel = document.getElementById('msg_errore');
 
 	// Aggiunta di un ascoltatore per l'invio del form
-	form.addEventListener('submit', (event) => {
-	// Prevenzione del comportamento predefinito del form (ricarica della pagina)
-	event.preventDefault();
-  
-	// Verifica dei dati inseriti
-	if (usernameInput.value.trim() === '') {
-		showError('Inserisci lo username');
-		return;
-	}else if(usernameInput.value.trim().includes(' ')){
-		showError("L'username non puo' contenere spazi");
-		return;	
-		//Altri controlli USERNAME
+	form.addEventListener('submit', accedi);
+	
+	
+	function accedi()  {
+	
+		// Verifica dei dati inseriti
+		/*if (usernameInput.value.trim() === '') {
+			showError('Inserisci lo username');
+			return;
+		}else*/ if(usernameInput.value.trim().includes(' ')){
+			showError("L'username non puo' contenere spazi");
+			return;	
+			//Altri controlli USERNAME
+		}
+		/*if (passwordInput.value.trim() === '') {
+			showError('Inserisci la password');
+			return;
+			//Altri controlli PASSWORD
+		}*/
+	
+		// Invio del form se i dati sono validi
+		else{
+
+			form.submit();
+		}
 	}
-	if (passwordInput.value.trim() === '') {
-		showError('Inserisci la password');
-		return;
-		//Altri controlli PASSWORD
-	}
-  
-	// Invio del form se i dati sono validi
-	form.submit();
-	});
 
 	// Funzione per visualizzare un messaggio di errore
 	function showError(message) {
-	errorLabel.textContent = message;
+		errorLabel.innerHTML = message;
 	}
 
 }
