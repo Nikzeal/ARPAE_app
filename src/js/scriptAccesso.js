7085e7b22dd1b724b3256193a6ab373aa4b571cc
Nikzeal <nicolo.lacara@aldini.istruzioneer.it>
2023-04-10 01:13:15 +0200

    sistemazione finale pagina accesso e aggiunta controllo validità password

--- src/js/scriptAccesso.js
+++ src/js/scriptAccesso.js
@@ -10,14 +10,16 @@ function init() {
 
 	// Aggiunta di un ascoltatore per l'invio del form
 	form.addEventListener("submit", controlli_input.bind(null, form, usernameInput, passwordInput, errorLabel) );
-
+	
 
 }
 
 
 function controlli_input(form, usernameInput, passwordInput, errorLabel)  {
 	
+		
 		// Verifica campi non vuoti (non necessario)
+		errorLabel.innerHTML = "";
 		if (usernameInput.value.trim() === '' || passwordInput.value.trim() === '') {
 			//showError('I campi sono obbligatori'); 
 			errorLabel.innerHTML = 'I campi sono obbligatori';
@@ -29,7 +31,22 @@ function controlli_input(form, usernameInput, passwordInput, errorLabel)  {
 			errorLabel.innerHTML = "L'username non puo' contenere spazi";
 			return;	
 		}
+		//verifica lunghezza password
+		else if(passwordInput.value.length < 8){
+			errorLabel.innerHTML = "La password deve essere di almeno 8 caratteri";
+			return;	
+		}
+		else{
+			form.reset();
+			sendData();
+		}
+		 
 		
+		
+}
+
+function sendData(){
+	console.log("mandare i dati");
 }
 
 /* Funzione per visualizzare un messaggio di errore. Non trovo necessario scrivere una funzione per stampare un messaggio
